040fd8e72df43f44e58128e4bfa6b3189fc4303b
Matteo Lamberti <72201009+BitMatt10111@users.noreply.github.com>
2021-11-17 21:30:27 +0100

    Update clientTCP_alphabot.py

--- clientTCP_alphabot.py
+++ clientTCP_alphabot.py
@@ -1,36 +1,12 @@
-#tcp
-#f"!list" -> f"list:{dict.keys()}"
-
 import socket as sck
 
 def main():
-    s = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-    s.connect(('192.168.0.131',5000))
+    s = sck.socket(sck.AF_INET, sck.SOCK_STREAM) #prepara il socket per la trasmissione TCP
+    s.connect(('192.168.0.131',5000)) #trova la connessione
 
     while True:
-        com = input("Inserisci comando e tempo:")
-        #pot1 = input("Inserisi potenza motore A:")
-        #pot2 = input("Inserisi potenza motore B:")
-        s.sendall(com.encode())
-
-        #risp = s.recv(4096)
-        #risp = risp.decode()
-        
-        #print(risp)
-
-        """
-        data = s.recvfrom(4096)
-        data = data.decode()
-        data = data.split(":")
-
-        if data[0] == 0:
-            print("Ho fatto bene")
-        elif data[1] == 1:
-            print("Ho sbagliato")
-            ris = input("Hai capito l'errore:")   #devo dire si --> 0 || no --> 1
-            s.sendall(ris.decode())
-        """
-
+        com = input("Inserisci nome comando:") #nome della senquenza sul database
+        s.sendall(com.encode()) #invia il comando
 
 if __name__=="__main__":
     main()
